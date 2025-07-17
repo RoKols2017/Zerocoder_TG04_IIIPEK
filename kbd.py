ecf8a0efb5d11f1c8403691c48786572bf894f36
Ivan Vorobev <ivan.vorobjov@gmail.com>
2025-07-17 11:51:24 +0300

    Worked TG04 bot

--- kbd.py
+++ kbd.py
@@ -1,4 +1,24 @@
 from aiogram.types import KeyboardButton, ReplyKeyboardMarkup, InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.utils.keyboard import InlineKeyboardBuilder, ReplyKeyboardBuilder
+
+async def build_keyboard(params,is_inline=True,url=False):
+    if is_inline:
+        print(params)
+        builder = InlineKeyboardBuilder()
+        for param in params:
+            if url:
+                builder.add(InlineKeyboardButton(text=param, url=params[param]))
+            else:
+                builder.add(InlineKeyboardButton(text=params[param], callback_data=param))
+        return builder.adjust(2).as_markup()
+    else:
+        builder = ReplyKeyboardBuilder()
+        for param in params:
+            builder.add(KeyboardButton(text=param))
+        return builder.adjust(2).as_markup(resize_keyboard=True,
+                                           one_time_keyboard=True,
+                                           input_field_placeholder="Выберите действие..."
+                                           )
 
 reply_kbd = ReplyKeyboardMarkup(
     keyboard=[
@@ -41,3 +61,18 @@ inline_kbd = InlineKeyboardMarkup(
         ],
     ]
 )
+
+
+start_kbd = ["Привет", "Пока",]
+
+url_kbd = {"Video":"https://rutube.ru/video/efbddc56a5b2d403bfb0bec1327ebe0f/",
+           "Music":"https://freemusicarchive.org/music/musinova/single/in-the-club-techno-housemp3/",
+           "News":"https://dzen.ru/news",
+           }
+
+more_kbd = {"show_more":"Больше",
+            }
+
+options_kbd = {"opt_1":"Опция 1",
+               "opt_2":"Опция 2",
+              }
--- main.py
+++ main.py
@@ -4,14 +4,14 @@ import os
 
 
 
-from aiogram import Bot, Dispatcher,types
+from aiogram import Bot, Dispatcher, types, F
 from aiogram.filters import Command, CommandStart
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import StatesGroup, State
 
 from dotenv import load_dotenv
 
-from kbd import reply_kbd
+from kbd import more_kbd, options_kbd, url_kbd, start_kbd, build_keyboard
 from utils import set_loglevel
 
 load_dotenv()
@@ -20,11 +20,39 @@ set_loglevel(level=os.getenv('LOG_LEVEL', 'INFO'))
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
-print
+
+@dp.message(Command(commands=['help']))
+async def process_help_command(message: types.Message):
+    await message.answer('''Я умею выполнять такие команды:
+    /start - Запустить бота
+    /help - Этот текст
+    /links - Список ссылок
+    /dynamic - Список опций
+    ''')
+
 @dp.message(CommandStart())
 async def process_start_command(message: types.Message):
-    await message.answer(text='Привет!', reply_markup=reply_kbd)
+    await message.answer(text='Привет!', reply_markup= await build_keyboard(start_kbd, is_inline=False))
 
+@dp.message(Command(commands=['links']))
+async def process_links_command(message: types.Message):
+    await message.answer(text='Список ссылок', reply_markup= await build_keyboard(url_kbd, is_inline=True, url=True))
+
+@dp.message(Command(commands=['dynamic']))
+async def process_dynamic_command(message: types.Message):
+    await message.answer(text='Динамические опции', reply_markup= await build_keyboard(more_kbd, is_inline=True))
+
+@dp.message(F.text.in_(start_kbd))
+async def process_start_callback(message: types.Message):
+    await message.answer(f"{message.text}, {message.from_user.full_name}!")
+
+@dp.callback_query(F.data.in_(more_kbd.keys()))
+async def process_more_callback(callback_query: types.CallbackQuery):
+    await callback_query.message.edit_text(text=f"Обновленные опции", reply_markup= await build_keyboard(options_kbd, is_inline=True))
+
+@dp.callback_query(F.data.in_(options_kbd.keys()))
+async def process_options_callback(callback_query: types.CallbackQuery):
+    await callback_query.message.answer(text=f"Вы выбрали {options_kbd[callback_query.data]}")
 
 
 if __name__ == '__main__':
